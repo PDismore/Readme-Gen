35f4b781e36924c24ca29238e64dd2401a43fadd
PDismore <pat.dismore@gmail.com>
2022-11-22 11:29:51 -0500

    added questions array to index

--- index.js
+++ index.js
@@ -4,7 +4,54 @@ const fs = require('fs')
 const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'what is the description of your project?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions of your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage of your project?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines of your project?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What are the test instructions of your project?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license is this project under?',
+        choices: ['MIT', 'GPL','LGPL', 'ISC', 'none']
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
